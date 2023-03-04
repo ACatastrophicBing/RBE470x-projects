eccf357cb3be564052d0f1612271de84dbe4345d
julpointy <julpoindexter@gmail.com>
2023-03-03 20:44:13 -0500

    alpha zero

--- team06/testcharacter.py
+++ team06/testcharacter.py
@@ -50,7 +50,7 @@ class TestCharacter(CharacterEntity):
         pass
 
     def q_learning(self,wrld):
-        alpha = 0.001
+        alpha = 0.0
         gamma = 0.9
         num_actions = 10
         num_f = 4
